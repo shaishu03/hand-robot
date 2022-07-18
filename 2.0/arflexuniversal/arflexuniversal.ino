c474503da47a52013f9d3ae02659438400c0daf0
Shaiel Fea <shaishuaf@gmail.com>
2022-07-18 01:10:23 -0300

    the open/closed measurement made in the setup is now as a function

--- 2.0/arflexuniversal/arflexuniversal.ino
+++ 2.0/arflexuniversal/arflexuniversal.ino
@@ -2,37 +2,37 @@
 #include <nRF24L01.h>
 #include <RF24.h>*/
 
-//declaración de patas
+//paws statement
 const int FLEXp_PIN = A1;
 const int FLEXi_PIN = A2;
 const int FLEXdm_PIN = A3;
 const int FLEXa_PIN = A4;
 const int FLEXm_PIN = A5;
 
-//variables de la definicion de valores
-int x1p = 0;
-int x1i = 0;
-int x1dm = 0;
-int x1a = 0;
-int x1m = 0;
+//variables of the definition of values
+int cp = 0;
+int ci = 0;
+int cdm = 0;
+int ca = 0;
+int cm = 0;
 
-int x2p = 1000;
-int x2i = 1000;
-int x2dm = 1000;
-int x2a = 1000;
-int x2m = 1000;
+int ap = 1000;
+int ai = 1000;
+int adm = 1000;
+int aa = 1000;
+int am = 1000;
 
 int a = 0;
 int c = 0;
 
-//transmisor
+//transmitter
 /*const int pinCE = 7;
 const int pinCSN = 8;
 RF24 tx(pinCE, pinCSN);
 
 const uint64_t pipe = 0xE8E8F0F0E1LL;*/
 
-//variables grado de cada dedo
+//variable degree of each finger
 int gradop; //pulgar
 int gradoi; //indice
 int gradodm; //del medio
@@ -41,77 +41,13 @@ int gradom; //menique
 
 int angulo[5];
 
+//fuction
+int open_closed_hand_measurement(int FLEXp, int FLEXi, int FLEXdm, int FLEXa, int FLEXm);
+
 void setup()
 {
   Serial.begin(9600);
-
-  Serial.println("MANTENGA LA MANO CERRADA HASTA QUE SE LE INDIQUE");
-  delay(6000);
-  Serial.println("ESPERE");
-  for (c=0; c<50; c++)
-  {
-    int flexADCp = analogRead(FLEXp_PIN);
-    int flexADCi = analogRead(FLEXi_PIN);
-    int flexADCdm = analogRead(FLEXdm_PIN);
-    int flexADCa = analogRead(FLEXa_PIN);
-    int flexADCm = analogRead(FLEXm_PIN);
-
-    if (x1p < flexADCp)  // x1 guarda el valor que quiero que sea 180
-    {
-      x1p = flexADCp;
-    }
-    if (x1i < flexADCi)
-    {
-      x1i = flexADCi;
-    }
-    if (x1dm < flexADCdm)
-    {
-      x1dm = flexADCdm;
-    }
-    if (x1a < flexADCa)
-    {
-      x1a = flexADCa;
-    }
-    if (x1m < flexADCm)
-    {
-      x1m = flexADCm;
-    }
-    delay(20);
-  }
-  Serial.println("AHORA MANTENGA LA MANO ABIERTA HASTA QUE SE LE INDIQUE");
-  delay(6000);
-  Serial.println("ESPERE");
-  for (a=0; a<50; a++)
-  {      
-    int flexADCp = analogRead(FLEXp_PIN);
-    int flexADCi = analogRead(FLEXi_PIN);
-    int flexADCdm = analogRead(FLEXdm_PIN);
-    int flexADCa = analogRead(FLEXa_PIN);
-    int flexADCm = analogRead(FLEXm_PIN);
-
-    if (x2p > flexADCp) // x2 guarda el valor que quiero que sea 0
-    {
-      x2p = flexADCp;
-    }
-    if (x2i > flexADCi)
-    {
-      x2i = flexADCi;
-    }
-    if (x2dm > flexADCdm)
-    {
-      x2dm = flexADCdm;
-    }
-    if (x2a > flexADCa)
-    {
-      x2a = flexADCa;
-    }
-    if (x2m > flexADCm)
-    {
-      x2m = flexADCm;
-    }
-    delay(20);
-  }
-  
+  open_closed_hand_measurement(FLEXp_PIN, FLEXi_PIN, FLEXdm_PIN, FLEXa_PIN, FLEXm_PIN);
   /*tx.begin();
   tx.openWritingPipe(pipe);*/
 }
@@ -124,19 +60,19 @@ void loop()
   int flexADCa = analogRead(FLEXa_PIN);
   int flexADCm = analogRead(FLEXm_PIN);
   
-  int angulop = map(flexADCp,x1p,x2p,180,0);
+  int angulop = map(flexADCp,cp,ap,180,0);
   angulop = constrain(angulop, 0, 180);
   
-  int anguloi = map(flexADCi,x1i,x2i,180,0);
+  int anguloi = map(flexADCi,ci,ai,180,0);
   anguloi = constrain(anguloi, 0, 180);
   
-  int angulodm = map(flexADCdm,x1dm,x2dm,180,0);
+  int angulodm = map(flexADCdm,cdm,adm,180,0);
   angulodm = constrain(angulodm, 0, 180);
   
-  int anguloa = map(flexADCa,x1a,x2a,180,0);
+  int anguloa = map(flexADCa,ca,aa,180,0);
   anguloa = constrain(anguloa, 0, 180);
   
-  int angulom = map(flexADCm,x1m,x2m,180,0);
+  int angulom = map(flexADCm,cm,am,180,0);
   angulom = constrain(angulom, 0, 180);
   
   //Serial.println("Bend: " + String(angulodm) + " degrees");
@@ -161,4 +97,75 @@ void loop()
   //  tx.write(angulo, sizeof (angulo));
   delay(1000);
   
+}
+
+int open_closed_hand_measurement(int FLEXp, int FLEXi, int FLEXdm, int FLEXa, int FLEXm)
+{
+  Serial.println("MANTENGA LA MANO CERRADA HASTA QUE SE LE INDIQUE");
+  delay(6000);
+  Serial.println("ESPERE");
+  for (c=0; c<50; c++)
+  {
+    int flexADCp = analogRead(FLEXp);
+    int flexADCi = analogRead(FLEXi_PIN);
+    int flexADCdm = analogRead(FLEXdm_PIN);
+    int flexADCa = analogRead(FLEXa_PIN);
+    int flexADCm = analogRead(FLEXm_PIN);
+
+    if (cp < flexADCp)  // x1 guarda el valor que quiero que sea 180
+    {
+      cp = flexADCp;
+    }
+    if (ci < flexADCi)
+    {
+      ci = flexADCi;
+    }
+    if (cdm < flexADCdm)
+    {
+      cdm = flexADCdm;
+    }
+    if (ca < flexADCa)
+    {
+      ca = flexADCa;
+    }
+    if (cm < flexADCm)
+    {
+      cm = flexADCm;
+    }
+    delay(20);
+  }
+  Serial.println("AHORA MANTENGA LA MANO ABIERTA HASTA QUE SE LE INDIQUE");
+  delay(6000);
+  Serial.println("ESPERE");
+  for (a=0; a<50; a++)
+  {      
+    int flexADCp = analogRead(FLEXp);
+    int flexADCi = analogRead(FLEXi);
+    int flexADCdm = analogRead(FLEXdm);
+    int flexADCa = analogRead(FLEXa);
+    int flexADCm = analogRead(FLEXm);
+
+    if (ap > flexADCp) // x2 guarda el valor que quiero que sea 0
+    {
+      ap = flexADCp;
+    }
+    if (ai > flexADCi)
+    {
+      ai = flexADCi;
+    }
+    if (adm > flexADCdm)
+    {
+      adm = flexADCdm;
+    }
+    if (aa > flexADCa)
+    {
+      aa = flexADCa;
+    }
+    if (am > flexADCm)
+    {
+      am = flexADCm;
+    }
+    delay(20);
+  }
+  return (cp, ci, cdm, ca, cm, ap, ai, adm, aa, am);
 }
